1e4addf9597e2ce5707068f1df79211d4bebe3a9
Gabriel Brito <mfkrgabriel@gmail.com>
2018-05-30 15:58:16 -0300

    Adding tests and searchs

--- source/search.js
+++ source/search.js
@@ -1,3 +1,18 @@
-export const search = (name) => 
+const searchByName = name => 
   fetch(`http://pokeapi.co/api/v2/pokemon/${name}`)
     .then(data => data.json())
+
+const searchByType = type => 
+  fetch(`http://pokeapi.co/api/v2/type/${type}`)
+    .then(data => data.json())
+
+const searchByAbility = ability => 
+  fetch(`http://pokeapi.co/api/v2/ability/${ability}`)
+    .then(data => data.json())
+
+
+export {
+  searchByName,
+  searchByType,
+  searchByAbility
+}
--- tests/search.spec.js
+++ tests/search.spec.js
@@ -1,5 +1,5 @@
 import chai, { expect } from 'chai';
-import { search } from '../source/search';
+import { searchByName, searchByType, searchByAbility } from '../source/search';
 import sinonChai from 'sinon-chai';
 import sinonStubPromise from 'sinon-stub-promise';
 import sinon from 'sinon';
@@ -22,41 +22,95 @@ describe('Search', () => {
     fetchedStub.restore();
   });
 
-
   describe('Smoke tests', () => {
     it('should exist search method', () => {
-      expect(search).to.exist;
+      expect(searchByName).to.exist;
+    });
+
+    it('should exist search method', () => {
+      expect(searchByType).to.exist;
+    });
+
+    it('should exist search method', () => {
+      expect(searchByAbility).to.exist;
     });
   });
 
 
-  describe('Search main tests', () => {
-    
+  describe('Search By Name tests', () => {   
     it('should call fetch function once', () => {
-      const pokemon = search();
+      const pokemon = searchByName();
       expect(fetchedStub).to.have.been.calledOnce;
     });
 
     it('should call fetch function with the correct URL', () => {
-      const pokemon = search('bulbasaur');
+      const pokemon = searchByName('bulbasaur');
       expect(fetchedStub).to.have.been
         .calledWith('http://pokeapi.co/api/v2/pokemon/bulbasaur');
 
-      const pokemon2 = search('squirtle');
+      const pokemon2 = searchByName('squirtle');
       expect(fetchedStub).to.have.been
         .calledWith('http://pokeapi.co/api/v2/pokemon/squirtle')
     });
 
-    it('should return an promise with JSON data within', () => {
+    it('should return an promise with JSON data', () => {
       promise.resolves({ body: 'json' });
 
-      const pokemon = search('bulbasaur');
-
+      const pokemon = searchByName('bulbasaur');
+  
       expect(pokemon.resolveValue).to.be.eql({ body: 'json' });
     });
-
   });
 
+  describe('Search By Type tests', () => {   
+    it('should call fetch function once', () => {
+      const type = searchByType();
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+
+    it('should call fetch function with the correct URL', () => {
+      const type = searchByType('grass');
+      expect(fetchedStub).to.have.been
+        .calledWith('http://pokeapi.co/api/v2/type/grass');
+
+      const type2 = searchByType('water');
+      expect(fetchedStub).to.have.been
+        .calledWith('http://pokeapi.co/api/v2/type/water')
+    });
+
+    it('should return an promise with JSON data', () => {
+      promise.resolves({ body: 'json' });
+
+      const type = searchByType('grass');
+  
+      expect(type.resolveValue).to.be.eql({ body: 'json' });
+    });
+  });
+
+  describe('Search By Ability tests', () => {   
+    it('should call fetch function once', () => {
+      const ability = searchByAbility();
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+
+    it('should call fetch function with the correct URL', () => {
+      const ability = searchByAbility('serene-grace');
+      expect(fetchedStub).to.have.been
+        .calledWith('http://pokeapi.co/api/v2/ability/serene-grace');
+
+      const ability2 = searchByAbility('stench');
+      expect(fetchedStub).to.have.been
+        .calledWith('http://pokeapi.co/api/v2/ability/stench')
+    });
+
+    it('should return an promise with JSON data', () => {
+      promise.resolves({ body: 'json' });
+
+      const ability = searchByAbility('serene-grace');
+  
+      expect(ability.resolveValue).to.be.eql({ body: 'json' });
+    });
+  });
 
 
 });
